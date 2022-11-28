ec2bb67d00faca0b080929cfa91712d311ed12c8
Klarab24 <118368155+Klarab24@users.noreply.github.com>
2022-11-28 14:15:45 +0000

    Week 1 Problem set 1 Mario

---
+++ week1/mario/more/mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+    int height = 0;
+    while (height > 8 || height <1){
+        printf("Height: ");
+        scanf("%i", &height);
+    }
+
+    for(int row = 1; row <= height; row++){
+        for(int ecolumn = 1; ecolumn<=(height-row); ecolumn++){
+            printf(" ");
+        }
+        for(int fcolumn =1; fcolumn<=row; fcolumn++){
+            printf("#");
+        }
+        printf(" ");
+        for(int fcolumn =1; fcolumn<=row; fcolumn++){
+            printf("#");
+        }
+        for(int ecolumn = 1; ecolumn<=(height-row); ecolumn++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
