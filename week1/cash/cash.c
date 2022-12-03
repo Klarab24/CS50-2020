ab3032038d35294866c6eb1eca4e7dca6756d2ac
UV <KJ@home.com>
2022-12-03 07:12:01 +0530

    Week 1 Problem: Cash Improved version

--- week1/cash/cash.c
+++ week1/cash/cash.c
@@ -10,45 +10,17 @@ int main(void){
     }
 
     cents = round(dollars*100);
-
-    int _25 = 25, _10 = 10, _5 = 5, _1 = 1;
+    int hard_change[] = {25, 10, 5, 1};
     int change = 0;
+
+    for(int i = 0; i < 4; i++){
+        if(cents == 0){
+            printf("%i\n", change);
+            return 0;
+        }
+        change += (int)cents/(hard_change[i]);
+        cents = cents%(hard_change[i]);
+    }
     
-    if(cents == 0){
-        printf("0");
-        return 0;
-    }
-    else{
-        change += (int)(cents/_25);
-        cents = cents%(_25);
-    }
-
-    if(cents == 0){
-        printf("%i\n", change);
-        return 0;
-    }
-    else{
-        change += (int)(cents/_10);
-        cents = cents%(_10);
-    }
-
-    if(cents == 0){
-        printf("%i\n", change);
-        return 0;
-    }
-    else{
-        change += (int)(cents/_5);
-        cents = cents%(_5);
-    }
-
-    if(cents == 0){
-        printf("%i\n", change);
-        return 0;
-    }
-    else{
-        change += (int)(cents/_1);
-        cents = cents%(_1);
-    }
-
     printf("%i\n", change);   
 }
